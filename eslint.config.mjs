8c1b076bcebaac6a198b12ef25c051067e4b5f3d
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2024-10-23 23:16:07 +0200

    updated .eslintrc.json with configuration data for linting Cypress tests:

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import cypressPlugin from "eslint-plugin-cypress";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    files: ["**/*.cy.js"], // Cypress test files
+    plugins: {
+      cypress: cypressPlugin,
+    },
+    rules: {
+      "cypress/no-unnecessary-waiting": "off", // Example of custom rule
+      "no-unused-vars": "off",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...cypressPlugin.configs.recommended.env, // Cypress globals
+      },
+    },
+  },
 ];
